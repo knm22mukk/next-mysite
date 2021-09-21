83d9b7cf9e5d49d14cea72740676157e0c0075ef
knm22mukk <knm22mukk@gmail.com>
2021-09-21 23:53:43 +0900

    トップページの表示がモバイルで崩れていたので修正

--- pages/index.js
+++ pages/index.js
@@ -13,46 +13,45 @@ export default function Home() {
           </Link>
         </div>
       </div>
-      <main className="py-16 container mx-auto px-6 md:px-0">
-        <section className="py-16 container mx-auto px-6 md:px-0">
-          <h1 className="text-3xl font-bold text-gray-600 mb-10">TextTextTextTextTextTextTextText</h1>
-          <div className="grid sm:grid-cols-3 gap-4 grid-cols-2">
-            <div>
-              <div className="bg-gray-300 h-44"></div>
-              <h3 className="text-lg font-semibold text-gray-500 mt-2">TextTextText<span className="text-gray-700">TextText</span></h3>
-            </div>
-            <div>
-              <div className="bg-gray-300 h-44"></div>
-              <h3 className="text-lg font-semibold text-gray-500 mt-2">TextTextText<span className="text-gray-700">TextText</span></h3>
-            </div>
-            <div>
-              <div className="bg-gray-300 h-44"></div>
-              <h3 className="text-lg font-semibold text-gray-500 mt-2">TextTextText<span className="text-gray-700">TextText</span></h3>
-            </div>
+      <section className="pt-10 container mx-auto px-6 md:px-0">
+        <h1 className="text-3xl font-bold text-gray-600 mb-10">Explore exotic locations in Finland</h1>
+        <div className="grid sm:grid-cols-3 gap-4 grid-cols-2">
+          <div>
+            <div className="bg-gray-300 h-44"></div>
+            <h3 className="text-lg font-semibold text-gray-500 mt-2">Saunas in <span className="text-gray-700">Helsinki</span></h3>
           </div>
-        </section>
-        <section>
-          <h1 className="inline-block text-gray-600 font-bold text-3xl mt-10">
-            TextTextTextTextText<br />
-            TextTextTextTextTextTextText
-          </h1>
+          <div>
+            <div className="bg-gray-300 h-44"></div>
+            <h3 className="text-lg font-semibold text-gray-500 mt-2">Saunas in <span className="text-gray-700">Rovaniemi</span></h3>
+          </div>
+          <div>
+            <div className="bg-gray-300 h-44"></div>
+            <h3 className="text-lg font-semibold text-gray-500 mt-2">Saunas in <span className="text-gray-700">Ruka</span></h3>
+          </div>
+        </div>
+        <hr className="w-40 my-10 border-4 rounded-md sm:mx-0 mx-auto" />
+      </section>
+      <section className="mb-5 container mx-auto px-6 md:px-0">
+        <h1 className="inline-block text-gray-600 font-bold text-3xl">
+          The holy sauna ritual <br />
+          (or how does Saunatime work).
+        </h1>
 
-          <div className="grid grid-cols-3 gap-4 mt-10">
-            <div>
-              <h3 className="text-lg font-semibold text-gray-500 mt-2">1. TextTextText</h3>
-              <p className="text text-gray-400">Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text</p>
-            </div>
-            <div>
-              <h3 className="text-lg font-semibold text-gray-500 mt-2">2. TextTextText</h3>
-              <p className="text text-gray-400">Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text</p>
-            </div>
-            <div>
-              <h3 className="text-lg font-semibold text-gray-500 mt-2">3. TextTextText</h3>
-              <p className="text text-gray-400">Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text</p>
-            </div>
+        <div className="grid grid-cols-3 gap-4 mt-10">
+          <div>
+            <h3 className="text-lg font-semibold text-gray-500 mt-2">1. Browse and book</h3>
+            <p className="text text-gray-400">Start by searching for a location. Once you find a sauna you like, simply check the availability, book it, and make a secure payment right away.</p>
           </div>
-        </section>
-      </main>
+          <div>
+            <h3 className="text-lg font-semibold text-gray-500 mt-2">2. Have a great bath</h3>
+            <p className="text text-gray-400">Meet your host on the date you chose and enjoy the home sauna experience. We'll handle the payment to the host after your experience.</p>
+          </div>
+          <div>
+            <h3 className="text-lg font-semibold text-gray-500 mt-2">3. Review the host</h3>
+            <p className="text text-gray-400">If you enjoyed the experience, let others know by giving a review to your sauna host. This way others will know where to go.</p>
+          </div>
+        </div>
+      </section>
     </Layout>
   )
 }
